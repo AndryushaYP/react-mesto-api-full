3fdd2e9666a74131e517886d2e999ee76dc798dc
Andrey Kloninger <veto.aion@gmail.com>
2020-12-20 21:04:10 +0500

    fix

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,14 +3,12 @@ const router = require('express').Router();
 const usersRouter = require('./users.js');
 const cardsRouter = require('./cards.js');
 
-const unknownPathRouter = router.use('*', () => {
+router.use(usersRouter, cardsRouter);
+
+router.use('*', () => {
   const error = new Error('Запрашиваемый ресурс не найден');
   error.statusCode = 404;
   throw error;
 });
 
-module.exports = router.use(
-  unknownPathRouter,
-  usersRouter,
-  cardsRouter,
-);
+module.exports = router;
